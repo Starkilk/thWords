c4746d3aec44b9b55c5500d04b96fdf3b4b30f84
Starlik <pavel.bolt@bk.ru>
2023-07-28 14:03:21 +0300

    Подключил фракгемт к FrameLayaut

--- app/src/main/java/com/pasha_yarik/mobileappthwords/MainActivity.kt
+++ app/src/main/java/com/pasha_yarik/mobileappthwords/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pasha_yarik.mobileappthwords
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pasha_yarik.mobileappthwords.databinding.ActivityMainBinding
+import com.pasha_yarik.mobileappthwords.fragments.Learn1Fragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -10,5 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().replace(R.id.placeHolder,Learn1Fragment.newInstance()).commit()
     }
 }
