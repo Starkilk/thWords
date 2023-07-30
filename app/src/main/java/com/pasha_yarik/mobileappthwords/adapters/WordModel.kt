0b12a5337ac0e0b34df25adf4ae2bc253af23ac5
Starlik <pavel.bolt@bk.ru>
2023-07-30 18:15:12 +0300

    Создал массив и Адаптер

---
+++ app/src/main/java/com/pasha_yarik/mobileappthwords/adapters/WordModel.kt
@@ -0,0 +1,8 @@
+package com.pasha_yarik.mobileappthwords.adapters
+
+data class WordModel(
+    var name:String,
+    var count:String,
+    var image:String? = null
+
+)
---
+++ app/src/main/java/com/pasha_yarik/mobileappthwords/adapters/WordsAdapter.kt
@@ -0,0 +1,44 @@
+package com.pasha_yarik.mobileappthwords.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.pasha_yarik.mobileappthwords.R
+import com.pasha_yarik.mobileappthwords.databinding.WordsListTemplateBinding
+
+class WordsAdapter:ListAdapter<WordModel,WordsAdapter.WordHolder >(MyComporator()) {
+
+    class WordHolder(view: View):RecyclerView.ViewHolder(view){
+        private val binding = WordsListTemplateBinding.bind(view)
+        fun setWord(word:WordModel) = with(binding){
+
+            tvNameCategory.text = word.name
+            val quantity = "Количество слов: ${word.count}"
+            tvCountWords.text = quantity
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.words_list_template,  parent,false)
+        return WordHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: WordHolder, position: Int) {
+        holder.setWord(getItem(position))
+    }
+
+    class MyComporator :DiffUtil.ItemCallback<WordModel>(){
+        override fun areItemsTheSame(oldItem: WordModel, newItem: WordModel): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: WordModel, newItem: WordModel): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+}
