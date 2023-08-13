5ea555a5cc4d5b0edec69037d9d59af9efa0f579
Starlik <pavel.bolt@bk.ru>
2023-08-13 13:30:32 +0300

    Логика профиля

--- app/src/main/java/com/pasha_yarik/mobileappthwords/FragmentsHomeY/FragmentHomeMainY.kt
+++ app/src/main/java/com/pasha_yarik/mobileappthwords/FragmentsHomeY/FragmentHomeMainY.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.pasha_yarik.mobileappthwords.R
 import com.pasha_yarik.mobileappthwords.databinding.FragmentHomeMainYBinding
+import com.pasha_yarik.mobileappthwords.utils.MainViewModel
 
 
 class FragmentHomeMainY : Fragment() {
     lateinit var binding: FragmentHomeMainYBinding
+    private val model: MainViewModel by activityViewModels()
+    private var homeProgress = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -19,8 +23,20 @@ class FragmentHomeMainY : Fragment() {
         return binding.root
     }
 
-    companion object {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        for(i in 0..39){
+            val modelProgrss = model.getPref(i.toString())
+            homeProgress += modelProgrss
+        }
+
+        binding.pbInHomeView.progress = homeProgress
+        val tmp = "${homeProgress} из 1000"
+        binding.tvCountWordsHome.text = tmp
+    }
+
+    companion object {
         @JvmStatic
         fun newInstance() = FragmentHomeMainY()
     }
