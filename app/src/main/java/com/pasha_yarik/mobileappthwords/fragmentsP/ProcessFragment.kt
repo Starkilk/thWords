cae45bfc7168b3b644428bf6ee7986397d6e77a5
Raimbl <kuz_y@inbox.ru>
2023-08-16 23:35:39 +0300

    soedinuil до конца

--- app/src/main/java/com/pasha_yarik/mobileappthwords/fragmentsP/ProcessFragment.kt
+++ app/src/main/java/com/pasha_yarik/mobileappthwords/fragmentsP/ProcessFragment.kt
@@ -120,10 +120,10 @@ class ProcessFragment : Fragment() {
                 33 -> processSlova(counterItem,resources.getStringArray(R.array.flora_i_fauna))
                 34 -> processSlova(counterItem,resources.getStringArray(R.array.priroda))
                 /*35 -> processSlova(0,resources.getStringArray(R.array.procc_array1))
-               36 -> processSlova(0,resources.getStringArray(R.array.procc_array1))
-               37 -> processSlova(0,resources.getStringArray(R.array.procc_array1))
-               38 -> processSlova(0,resources.getStringArray(R.array.procc_array1))
-               39 -> processSlova(0,resources.getStringArray(R.array.procc_array1))*/
+               36 -> processSlova(0,resources.getStringArray(R.array.procc_array1)) */
+               37 -> processSlova(counterItem,resources.getStringArray(R.array.dom))
+               38 -> processSlova(counterItem,resources.getStringArray(R.array.pribori))
+               39 -> processSlova(counterItem,resources.getStringArray(R.array.veshi))
             }
 
         }
